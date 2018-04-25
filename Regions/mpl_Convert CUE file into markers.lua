5b30de2d6b19aef60602d21ec2a51ed1edcbe632
FishSatire <38734998+FishSatire@users.noreply.github.com>
2018-04-25 20:00:58 +0100

    Update mpl_Convert CUE file into markers.lua
    Fixed script:
    - to allow ReplayGain in the cue sheet
    - to take Index 01 not 00 (pre-Index)
    - remove performer info as it's not always present per track but as overall album - should use if statement really
    - changed to use /75 frames on CD not /100 ms

--- Regions/mpl_Convert CUE file into markers.lua
+++ Regions/mpl_Convert CUE file into markers.lua
@@ -1,5 +1,5 @@
 -- @version 1.01
--- @author MPL
+-- @author MPL, edits by FishSatire
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @description Convert CUE file into markers
 -- @changelog
@@ -22,24 +22,23 @@
     -- collect content
       local t, id = {}, 0
       for data in content:gmatch('[^\r\n]+') do 
-        if data:find('TRACK') then id = id +1 end
+        if data:find('TRACK ') then id = id +1 end -- without space after TRACK, ReplayGain_TRACK_GAIN breaks script
         if not t[id] then t[id] = '' end
         t[id] = t[id]..data 
       end
     -- parse data
       regt = {}
       for i = 1, #t do
-        local time = t[i]:match('INDEX [%d]+ ([%d%p]+)')
+        local time = t[i]:match('INDEX 01 ([%d%p]+)') -- specify Index 01, not pre-Index 00
         local pos = 0
         if time:match('[%d]+%:[%d]+%:[%d]+') then
-          pos = time:match('([%d]+)%:[%d]+%:[%d]+')*60 + time:match('[%d]+%:([%d]+)%:[%d]+') + time:match('[%d]+%:[%d]+%:([%d]+)')/100
+          pos = time:match('([%d]+)%:[%d]+%:[%d]+')*60 + time:match('[%d]+%:([%d]+)%:[%d]+') + time:match('[%d]+%:[%d]+%:([%d]+)')/75 -- 75 frames in a CD not 100
         end
         regt[i] = {title = t[i]:match('TITLE "(.-)"'),
-                   perf = t[i]:match('PERFORMER "(.-)"'), 
-                   pos = pos}
+                   pos = pos} -- remove Performer as it's not always included, could use if statement
       end
     --  add markers
-      for i = 1, #regt do AddProjectMarker( 0, false, regt[i].pos, -1, regt[i].perf..' - '..regt[i].title, -1 ) end
+      for i = 1, #regt do AddProjectMarker( 0, false, regt[i].pos, -1, regt[i].title, -1 ) end -- removed performer
       UpdateTimeline()
   end
   retval, fn =  GetUserFileNameForRead('', scr, '.cue' )
